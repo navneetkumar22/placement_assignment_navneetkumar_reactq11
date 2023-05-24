fd1f10624517508ce0c263a3f912a981c43762cf
Navneet Kumar <navneetsinghpanwar90@gmail.com>
2023-05-24 14:52:54 +0530

    react question 11

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const addTodo = () => {
+    if (newTodo !== '') {
+      const todo = {
+        id: Date.now(),
+        title: newTodo
+      };
+      setTodos([...todos, todo]);
+      setNewTodo('');
+    }
+  }
+
+  const deleteTodo = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className='heading'>Todo  App</h1>
+
+      <div className='new-todo'>
+        <input type='text' placeholder='Enter new todo' value={newTodo} onChange={e => setNewTodo(e.target.value)} />
+        <button className='add' onClick={addTodo}>Add Todo</button>
+      </div>
+
+      <hr />
+      <h2>All Todos</h2>
+
+      <div className='todos'>
+        {todos.map((todo) => (
+          <div className='todo' key={todo.id}>
+            <p>{todo.title}</p>
+            <button className='delete' onClick={() => deleteTodo(todo.id)}>Delete Todo</button>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
